b994e23bdc508e139564ed15edd3daaf90380bcd
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2015-04-06 11:24:57 -0400

    make test look for concorde in -Dimagej.app.directory=$HOME/packages/Fiji.app which is consistent with it installing the artifact in the Fiji installation (i.e. the test confirms that the installed jar works where it was installed)

--- src/test/java/org/janelia/sort/tsp/TSPTest.java
+++ src/test/java/org/janelia/sort/tsp/TSPTest.java
@@ -25,28 +25,39 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TSPTest {
-	
+
 	final String path                                      = "src/test/java/org/janelia/sort/tsp/AVG_inlier ratio matrix-excerpt.tif";
 	final DataToStringFullMatrixTSPLIB converter           = new DataToStringFullMatrixTSPLIB();
 	final SimilarityToDistanceSigmoid similarityToDistance = new SimilarityToDistanceSigmoid( 1000.0, 0.0, 1000000.0 );
 	final String referencePath                             = "src/test/java/org/janelia/sort/tsp/excerpt-tsp.dat";
-	
+
 	final String matrixWithHolesPath    = "src/test/java/org/janelia/sort/tsp/AVG_inlier ratio matrix.tif";
 	final String matrixWithoutHolesPath = "src/test/java/org/janelia/sort/tsp/AVG-no-empty.tif";
-	
-	// TODO for now, concorde needs to be present in some directory in PATH env variable;
-	// TODO set PATH variable in eclipse by 
-	// TODO Run -> Run Configurations -> Environment -> New { "Variable" : "PATH", "Value" : "/path/to/directory/containing/concorde" }
-	// TODO this might not be platform robust and needs to be replaced by sth. else
-	final String concordeExecutablePath = "concorde";
+
+	// Concorde must be present in either some directory in PATH env variable
+	// or in <IMAGEJ_APP_DIR>/bin/.  <IMAGEJ_APP_DIR> can be made available to maven
+	// by calling it e.g.
+	//
+	// mvn -Dimagej.app.directory=$HOME/packages/Fiji.app/
+	//
+	// which also installs the artifact in <IMAGEJ_APP_DIR>/jar
+	// Alternative: set PATH variable in eclipse by
+	// Run -> Run Configurations -> Environment -> New { "Variable" : "PATH", "Value" : "/path/to/directory/containing/concorde" }
+	private String imageJAppDir;
+	{
+        imageJAppDir = System.getProperty( "imagej.app.directory" );
+        imageJAppDir = imageJAppDir == null ? "" : imageJAppDir + "bin/";
+        System.out.println( "ImageJAppDir = " + imageJAppDir );
+	}
+	final String concordeExecutablePath = imageJAppDir + "concorde";
 	final String inputFileName          = "src/test/java/org/janelia/sort/tsp/excerpt-tsp.dat";
 	final String outputFileName         = "src/test/java/org/janelia/sort/tsp/excerpt-result.txt";
 	final String referenceMatrixPath    = "src/test/java/org/janelia/sort/tsp/excerpt-result.tif";
-	
+
 	final int concordeSeed     = 10;
 	// final int[] orderReference = new int[]{0, 21, 20, 19, 18, 17, 7, 6, 5, 10, 9, 8, 13, 12, 11, 16, 15, 14, 4, 3, 2, 1};
 	final int[] orderReference = new int[]{0, 1, 2, 3, 4, 14, 15, 16, 11, 12, 13, 8, 9, 10, 5, 6, 7, 17, 18, 19, 20, 21};
-	
+
 
 	@Test
 	public void testConvertMatrix() {
@@ -54,7 +65,7 @@ public class TSPTest {
 		final Img<FloatType> img = ImageJFunctions.wrapFloat( imp );
 		final String s           = TSP.convertMatrix( img, converter, similarityToDistance );
 		final String[] split     = s.split( System.getProperty("line.separator") );
-		
+
 		LineNumberReader lnr;
 		try {
 			lnr = new LineNumberReader(new FileReader(new File( referencePath )));
@@ -66,7 +77,7 @@ public class TSPTest {
 		} catch ( final IOException e ) {
 			Assert.fail( "Error accessing file " + referencePath );
 		}
-		
+
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader( new File( referencePath ) ) );
@@ -74,50 +85,50 @@ public class TSPTest {
 			br = null;
 			Assert.fail( "Could not find file " + referencePath );
 		}
-		
+
 		String line = null;
 		try {
-			for (int index = 0; (line = br.readLine()) != null; ++index )  
+			for (int index = 0; (line = br.readLine()) != null; ++index )
 			{
 				Assert.assertEquals( split[index], line );
 			}
 		} catch (final IOException e) {
 			Assert.fail( "Error accessing file " + referencePath );
-		} 
-		
+		}
+
 	}
-	
+
 	@Test
 	public void testCleanMatrix() {
 		final FloatImagePlus<FloatType> matrixWithHoles             = ImagePlusAdapter.wrapFloat( new ImagePlus( matrixWithHolesPath ) );
 		final FloatImagePlus<FloatType> matrixWithoutHolesReference = ImagePlusAdapter.wrapFloat( new ImagePlus( matrixWithoutHolesPath ) );
-		
 
-		
+
+
 		final ArrayList<Long> keptIndices              = new ArrayList< Long >();
 		final ArrayList<Long> removedIndices           = new ArrayList< Long >();
 		final ArrayList<ArrayList<Long>> badSuccessors = new ArrayList< ArrayList< Long > >();
 		final boolean[] sectionStatus                  = new boolean[ (int) matrixWithHoles.dimension( 0 ) ];
-		
+
 		final RandomAccessibleInterval<FloatType> matrixWithoutHoles = TSP.cleanMatrix( matrixWithHoles, removedIndices, keptIndices, badSuccessors, sectionStatus );
-		
+
 		Assert.assertEquals( matrixWithoutHolesReference.dimension( 0 ), keptIndices.size() );
-		
+
 		Assert.assertEquals( matrixWithHoles.dimension( 0 ), keptIndices.size() + removedIndices.size() );
-		
+
 		for ( int d = 0; d < matrixWithoutHoles.numDimensions(); ++d )
 			Assert.assertEquals( matrixWithoutHoles.dimension( d ), matrixWithoutHolesReference.dimension( d ) );
-		
+
 		final Cursor<FloatType> ref = Views.flatIterable( matrixWithoutHolesReference ).cursor();
 		final Cursor<FloatType> res = Views.flatIterable( matrixWithoutHoles ).cursor();
 		while( ref.hasNext() ) {
 			Assert.assertEquals( res.next(), ref.next() );
 		}
 	}
-	
+
 	@Test
 	public void testTSPSolver() {
-		
+
 		try {
 			TSP.runConcordeTSPSolver( concordeExecutablePath, inputFileName, outputFileName, String.format("-s %d", concordeSeed ) );
 		} catch (final IOException e) {
@@ -126,22 +137,22 @@ public class TSPTest {
 		final int[] result = TSP.tspResultToArray( outputFileName, 22 );
 		Assert.assertArrayEquals( orderReference, result );
 	}
-	
-	
+
+
 	@Test
 	public void testRearrangement() {
-		
+
 		final FloatImagePlus<FloatType> input            = ImagePlusAdapter.wrapFloat( new ImagePlus( path ) );
 		final FloatImagePlus<FloatType> reference        = ImagePlusAdapter.wrapFloat( new ImagePlus( referenceMatrixPath) );
 		final RandomAccessibleInterval<FloatType> output = TSP.rearrangeMatrix( input, orderReference );
-		
+
 		final Cursor<FloatType> o = Views.flatIterable( output ).cursor();
 		final Cursor<FloatType> r = Views.flatIterable( reference ).cursor();
-		
+
 		while( o.hasNext() ) {
 			Assert.assertEquals( r.next(), o.next() );
 		}
-		
+
 	}
 
 }
